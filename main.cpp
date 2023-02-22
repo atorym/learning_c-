073b2a2dcc973d1561359fdf70d4fb977782a10b
atorym <atorym@gmail.com>
2023-02-22 14:22:49 +0500

    clean master

--- main.cpp
+++ main.cpp
@@ -1,27 +1,9 @@
 #include <iostream> 
-/*
-заходит из консоли в git bash
-bash
 
-переключение между ветками
-git checkout <название ветки>
-
-создание новой ветки
-git checkout master && git checkout -b "<название новой ветки>"
-
-проиндексировать изменения перед фиксацией
-git add .
-
-создание фиксации на ветке
-git commit -m "<сообщение к коммиту>"
-сохраняет текущее состояние репозитория
-ctrl+k ctrl+k
-
-новый коммит создается комбинацией
-git add . && git commit -m "<сообщение к коммиту>"
-*/
 
 int main() {
-
-  return EXIT_SUCCESS;
+  int a;
+  std::cin >> a;
+  std::cout << std::endl << a;
+  return 0;
 }

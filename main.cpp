ab7ff0ae8e083edb54a9a999c61f388c35abc281
atorym <atorym@gmail.com>
2023-03-12 21:15:44 +0500

    Программа_по_определению_ASCII_кода_по_литере

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
-#include <iostream> 
+#include <iostream>
 
 
 int main() {
+#ifdef WIN32
+  setlocale(LC_ALL, "Russian");
+#endif
 
-  return 0;
+  std::wcout << L"Программа по определению ASCII кода по литере.\n\n"
+             << L"Введите желаемую литеру или сочетание символов\n"
+             << L"(для завершения программы введите символ <+>) :\n";
+  char ch;
+  int i{0};
+  while (i < 100) {
+    std::cin >> ch;
+    if (ch == '+') {
+      i = 100;
+    } else {
+      std::wcout << L"Ваша литера <" << ch
+                 << L"> имеет следующий \n ASCII код "
+                 << static_cast<int>(ch) << "." << std::endl;
+    }
+  }
+
+  return EXIT_SUCCESS;
 }

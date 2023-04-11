336e9e3cdcd9f2e655c219cf1a40c6a99680387e
atorym <atorym@gmail.com>
2023-04-11 15:28:46 +0500

    Тест_урок_57

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
-
+#include <string>
 
 int main() {
-
-
-  return 0;
+  {
+    using namespace std;
+    cout << "Enter your full name\n";
+    string name;
+    getline(cin, name);
+    cout << "Enter your age\n";
+    int age;
+    cin >> age;
+    cout << "Your name is " << name << ", in your name " << name.length() - 1 << " characters.\n"
+         << "You've lived " << static_cast<double>(age) / (name.length() - 1.0) << " years for each letter in your name.";
+  }
+  return EXIT_SUCCESS;
 }

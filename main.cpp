aa0e975eb01627e9ac058e0e9efbdaec07e65bec
conelov <conelove@yandex.ru>
2023-04-16 18:13:13 +0300

    Тест_урок_61_Задание_2 review

--- main.cpp
+++ main.cpp
@@ -7,31 +7,39 @@
 результат в виде десятичного числа.
 */
 
-#include <iostream>
-#include <stdint.h>
+#include <cstdint>
 #include <iomanip>
+#include <iostream>
 
+
+// натуральная дробь
 struct fraction {
-  std::uint32_t numerator{};
-  std::uint32_t denominator{};
+  std::uint32_t numerator;
+  std::uint32_t denominator;
 };
 
-void multiply(fraction lhs, fraction rhs) {
-  double result{static_cast<double >(lhs.numerator*rhs.numerator)/(lhs.denominator*rhs.denominator)};
-  std::cout << "Result of multiplying your fractions is: " << std::setprecision(3) << result << "\n";
+
+//double multiply(fraction lhs, fraction rhs) {
+//  return static_cast<double>(lhs.numerator * rhs.numerator) / (lhs.denominator * rhs.denominator);
+//}
+
+
+fraction multiply(fraction lhs, fraction rhs) {
+  return {lhs.numerator * rhs.numerator, lhs.denominator * rhs.denominator};
 }
 
-int main() {
 
-  using std::cin;
-  using std::cout;
-  cout << "Pls enter numerator, denominator of first fraction:\n";
-  fraction lhs{};
-  cin >> lhs.numerator >> lhs.denominator;
-  cout << "Pls enter numerator, denominator of second fraction:\n";
-  fraction rhs{};
-  cin >> rhs.numerator >> rhs.denominator;
-  multiply(lhs, rhs);
+int main() {
+  std::cout << "Pls enter numerator, denominator of first fraction:\n";
+  fraction lhs;
+  std::cin >> lhs.numerator >> lhs.denominator;
+  std::cout << "Pls enter numerator, denominator of second fraction:\n";
+  fraction rhs;
+  std::cin >> rhs.numerator >> rhs.denominator;
+
+  auto result = multiply(lhs, rhs);
+  std::cout << "Result of multiplying your fractions is: " << std::setprecision(3)
+            << result.numerator / result.denominator << "\n";
 
   return EXIT_SUCCESS;
 }

b008f3b7c80f7ad4fe49a9356b14f00d57e95117
conelov <conelove@yandex.ru>
2023-04-16 17:35:08 +0300

    Тест_урок_57 review

--- main.cpp
+++ main.cpp
@@ -12,18 +12,17 @@ You've lived 5.625 years for each letter in your name.
 
 #include <iostream>
 #include <string>
+#include <cstdint>
+
 
 int main() {
-  {
-    using namespace std;
-    cout << "Enter your full name\n";
-    string name;
-    getline(cin, name);
-    cout << "Enter your age\n";
-    int age;
-    cin >> age;
-    cout << "Your name is " << name << ", in your name " << name.length() - 1 << " characters.\n"
-         << "You've lived " << static_cast<double>(age) / (name.length() - 1.0) << " years for each letter in your name.";
-  }
+  std::cout << "Enter your full name\n";
+  std::string name;
+  std::getline(std::cin, name);
+  std::cout << "Enter your age\n";
+  std::uint8_t age;
+  std::cin >> age;
+  std::cout << "Your name is " << name << ", in your name " << name.length() - 1 << " characters.\n"
+            << "You've lived " << static_cast<double>(age) / (name.length() - 1.) << " years for each letter in your name.";
   return EXIT_SUCCESS;
 }

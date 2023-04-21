57c99a17022e964e27e3741142877ef4835d52f2
atorym <atorym@gmail.com>
2023-04-21 14:37:36 +0500

    Тест_урок_65_Задание_1

--- main.cpp
+++ main.cpp
@@ -1,8 +1,38 @@
-#include <iostream> 
+/*
+Задание №1
+Напишите функцию calculate(), которая принимает две переменные типа int и одну переменную типа char,
+которая, в свою очередь, представляет одну из следующих математических операций: +, -, *, / или %
+(остаток от числа). Используйте switch для выполнения соответствующей математической операции над целыми числами,
+а результат возвращайте обратно в main(). Если в функцию передается недействительный математический оператор,
+то функция должна выводить ошибку. С оператором деления выполняйте целочисленное деление.
+*/
+
+#include <cstdint>
+#include <iostream>
 
 
+std::int16_t calculate(std::int16_t lhs, std::int16_t rhs, char operation) {
+  switch (operation) {
+    case '+' :
+      return lhs + rhs;
+      break;
+    case '-' :
+      return lhs - rhs;
+      break;
+    case '*' :
+      return lhs * rhs;
+      break;
+    case '/' :
+      return lhs / rhs;
+      break;
+    default:
+      std::cout << "ERROR";
+      break;
+  }
+}
 int main() {
 
+std::cout << calculate(32,16,'/');
 
-  return 0;
+return 0;
 }

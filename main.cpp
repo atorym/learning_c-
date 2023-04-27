9b9104b140ac8cc719e9a7b4cc92051d6f051880
atorym <atorym@gmail.com>
2023-04-28 00:44:20 +0500

    Тест_урок_67_Задание_3

--- main.cpp
+++ main.cpp
@@ -1,8 +1,31 @@
-#include <iostream> 
+/*
+Задание 3
+Измените программу из последнего подраздела «Вложенные циклы while»
+так, чтобы она выводила следующее:
+5 4 3 2 1
+4 3 2 1
+3 2 1
+2 1
+1
+ */
+
+#include <iostream>
+#include <cstdint>
 
 
-int main() {
-
+int main()
+{
+  uint16_t outer = 5;
+  while (outer >= 1)
+  {
+    uint16_t inner = outer;
+    while (inner >= 1) {
+      std::cout << inner-- << " ";
+    }
+    // Вставляем символ новой строки в конце каждого ряда
+    std::cout << "\n";
+    --outer;
+  }
 
   return 0;
 }

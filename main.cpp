a1c43a4801fceeaed78213714eeb61f247c67b55
atorym <atorym@gmail.com>
2023-05-02 20:52:01 +0500

    Тест_урок_69_Задание_1

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
-#include <iostream> 
+/*
+Задание №1
+Напишите цикл for, который выводит каждое четное число в диапазоне от 0 до 20.
+ */
 
+
+#include <cstdint>
+#include <iostream>
 
 int main() {
 
+  for (std::uint16_t number = 0; number <= 20; number += 2) {
+    std::cout << number << " ";
+  }
 
   return 0;
 }

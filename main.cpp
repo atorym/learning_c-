545f8d1bccaa0d33de0445494660da65000ae76d
atorym <atorym@gmail.com>
2023-05-21 03:23:35 +0500

    Итоговый_тест_5_глава_2_задание_рефакторинг

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 Во-первых, ваша программа должна выбрать случайное целое число в диапазоне от 1 до 100.
 Пользователю дается 7 попыток, чтобы угадать это число.
 Если пользователь не угадал число, то программа должна подсказывать, была ли его
-догадка слишком большой или слишком маленькой.
+догадка слишком большой или слишком маленькой. Если пользователь out of range, то количество попыток не уменьшается.
 Если пользователь угадал число, то программа должна сообщить, что всё верно — вы выиграли.
 Если же у пользователя кончились попытки, и он не угадал число, то программа должна сообщить
 ему, что он проиграл и показать правильный результат.
@@ -14,75 +14,92 @@
  */
 
 #include <cstdint>
-#include <cstdlib>
-#include <ctime>
 #include <iostream>
+#include <random>
 
 
-std::int16_t randomizer();
-bool comparison(std::int16_t, std::int16_t);
-std::uint8_t check_input();
+using user_value_t = std::uint8_t;
+enum class user_input_state:std::uint8_t;
+
+user_value_t randomiser();
+user_input_state comparison(user_value_t, user_value_t);
+bool needed_more_request();
+void game_session();
 
 
 int main() {
-  srand(static_cast<std::uint16_t>(std::time(0)));
-  std::cout << "Program Hi-Lo:\n";
-start_again:
-  std::cout << "I'm create random int number in range of 0-100. You have 7 try's. Can you guess it?\n";
-  std::int16_t random_number = randomizer();
-  std::int8_t counter{0};
-  while (counter < 7) {
-    std::int16_t user_number;
-    std::cin >> user_number;
-    if (comparison(random_number, user_number)) {
-      std::cout << "You win!\n";
-      break;
-    }
-    ++counter;
+  std::cout << "\tProgram Hi-Lo:\n";
+  for (bool needed_more = true; needed_more; needed_more = needed_more_request()) {
+    game_session();
   }
-repeat:
-  std::uint8_t decision = check_input();
-  if (decision == 'y') {
-    goto start_again;
-  } else if (decision == 'n') {
-    return EXIT_SUCCESS;
-  } else {
-    goto repeat;
-  }
+  return EXIT_SUCCESS;
 }
 
 
-std::int16_t randomizer() {
-  static const double fraction = 1.0 / (static_cast<double>(RAND_MAX) + 1.0);
-  return static_cast<int>(rand() * fraction * (100 - 0 + 1) + 0);
+user_value_t randomiser() {
+  std::random_device rd;
+  static std::mt19937 random_engine{rd()};
+  return std::uniform_int_distribution<user_value_t>{0, 100}(random_engine);
 }
 
 
-bool comparison(std::int16_t random_number, std::int16_t user_number) {
-  bool status;
-  if (user_number < 0 || user_number > 100) {
-    std::cout << "Your number is out of range!\n";
-    status = false;
+enum class user_input_state:std::uint8_t {
+  win,
+  out_of_range,
+  bigger,
+  smaller
+};
+
+
+user_input_state comparison(user_value_t random_number, user_value_t user_number) {
+  if (user_number == random_number) {
+    return user_input_state::win;
+  } else if (user_number < 1 || user_number > 100) {
+    return user_input_state::out_of_range;
   } else if (user_number > random_number) {
-    std::cout << "Your number is bigger then random.\n";
-    status = false;
-  } else if (user_number < random_number) {
-    std::cout << "Your number is smaller then random.\n";
-    status = false;
-  } else {
-    std::cout << "Great! You got it!\n";
-    status = true;
+    return user_input_state::bigger;
+  } else /* if (user_number < random_number) */ {
+    return user_input_state::smaller;
   }
-  return status;
 }
 
 
-std::uint8_t check_input() {
-  std::cout << "Would you like to try again?(y/n)\n";
-  std::uint8_t decision;
-  std::cin >> decision;
-  if (decision == 'y' || decision == 'n')
-    return decision;
-  else
-    std::cout << "Input is invalid, pls try again!\n";
+bool needed_more_request() {
+  std::cout << "Wanna fuck more?(y/n): ";
+  std::string str;
+  while (true) {
+    std::cin >> str;
+    if (str == "y") {
+      return true;
+    } else if (str == "n") {
+      return false;
+    }
+    std::cout << "invalid input. Try again.(y/n): ";
+  }
+}
+
+
+void game_session() {
+  std::cout << "I'm create random int number in range of 0-100. You have 7 try's. Can you guess it?\n";
+  user_value_t const random_number = randomiser();
+  std::size_t i{};
+  do {
+    std::uint16_t user_number;
+    std::cin >> user_number;
+    switch (comparison(random_number, user_number)) {
+      case user_input_state::win:
+        std::cout << "Great, You won!\n";
+        return;
+      case user_input_state::out_of_range:
+        std::cout << "Dear boy, You are balbes!\n";
+        continue;
+      case user_input_state::bigger:
+        std::cout << "Your number is bigger then random\n";
+        break;
+      case user_input_state::smaller:
+        std::cout << "Your number is smaller then random\n";
+        break;
+    }
+    ++i;
+  } while (i < 7);
 }

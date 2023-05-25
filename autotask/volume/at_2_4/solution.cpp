3e329f691514d90fe58f720539860d559369e014
atorym <atorym@gmail.com>
2023-05-25 23:41:14 +0500

    at 2.1 at 2.4 answer

--- autotask/volume/at_2_4/solution.cpp
+++ autotask/volume/at_2_4/solution.cpp
@@ -1,3 +1,8 @@
+/*
+ * Напишите программу, которая считывает числа с клавиатуры до тех пор, пока не будет введено отрицательное число.
+ * После окончания ввода, программа должна вывести сумму всех положительных чисел, среднее арифметическое и максимальное из них.
+ */
+
 #include <at_2_4/solution.hpp>
 
 
@@ -5,7 +10,25 @@ namespace at::at_2_4 {
 
 
 answer solution(std::istringstream&& iss) {
-  return <#initializer#>;
+  answer_value current_value;
+  answer out{};
+  std::size_t counter{};
+  while (true) {
+    iss >> current_value;
+
+    if (current_value<0){
+      break;
+    }
+
+    out.sum += current_value;
+    counter++;
+    out.average=out.sum/counter;
+
+    if(current_value>out.max){
+      out.max=current_value;
+    }
+  }
+  return out;
 }
 
 

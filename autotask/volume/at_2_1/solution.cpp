5a93f7d552aa4016ee2ab58beb60238aceb917c5
conelov <conelove@yandex.ru>
2023-05-25 13:26:32 +0300

    at_2_1

--- autotask/volume/at_2_1/solution.cpp
+++ autotask/volume/at_2_1/solution.cpp
@@ -5,7 +5,13 @@ namespace at::at_2_1 {
 
 
 answer solution(arg_t a, arg_t b, arg_t c) {
-  return <#initializer #>;
+  if (a == b && a == c) {
+    return answer::equilateral;
+  }
+  if (a == b || b == c || a == c) {
+    return answer::isosceles;
+  }
+  return answer::scalene;
 }
 
 

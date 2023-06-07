e809d92fd4335be08274fbe5c90183974d86dc53
atorym <atorym@gmail.com>
2023-06-07 19:34:33 +0500

    Bubble sort realisation

--- sort_sandbox/volume/bubble.cpp
+++ sort_sandbox/volume/bubble.cpp
@@ -1,12 +1,21 @@
 #include <sandbox_sort/defines.hpp>
-
+#include <utility>
 
 namespace at::sandbox_sort {
 
 
 array_t sort(array_t in) {
- <#initializer#>
+  const std::size_t size_of_array = std::size(in);
+
+  for (std::size_t i = 0; i < size_of_array - 1; ++i) {
+    for (std::size_t j = i+1; j < size_of_array; ++j){
+      if (in[i] > in[i + 1]) {
+        std::swap(in[i], in[j]);
+      }
+    }
+  }
+  return in;
 }
 
 
-} // namespace at::sandbox_sort
+}// namespace at::sandbox_sort

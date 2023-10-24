313545f0a68522d4293bfa15628a33dd29b8b510
conelov <conelove@yandex.ru>
2023-10-24 17:28:53 +0300

    review

--- main.cpp
+++ main.cpp
@@ -6,44 +6,46 @@
 * вызывает функцию для сортировки имен в алфавитном порядке;
 * выводит отсортированный список имен.
 */
+#include <algorithm>
 #include <cstdint>
 #include <cstring>
 #include <iostream>
+#include <limits>
 #include <string>
 
 
 int main() {
 
-  std::cout << "How many names would you like to enter?" << '\n';
-  std::int16_t user_count;
+  std::cout << "How many names would you like to enter?\n";
+  std::size_t user_count;
   while (true) {
     std::cin >> user_count;
     if (user_count <= 0) {
-      std::cout << "Pls repeat." << '\n';
+      std::cout << "Pls repeat.\n";
     } else {
       break;
     }
   }
   //инициализация массива имен
-  auto* name_array = new std::string[user_count]{};
-  std::cin.ignore(32767, '\n');
-  for (std::int16_t i = 0; i < user_count; i++) {
+  auto const name_array = new std::string[user_count];
+  std::cin.ignore(std::numeric_limits<bool>::max(), '\n');
+  for (std::size_t i = 0; i < user_count; ++i) {
     std::cout << "Pls enter " << i + 1 << " name:" << '\n';
     std::getline(std::cin, name_array[i]);
   }
 
   //сортировка массива в алфавитном порядке
-  for (std::int16_t i = 0; i < user_count - 1; i++) {
-    for (int j = 0; j < user_count - 1; j++) {
+  for (std::size_t i = 0; i < user_count - 1; ++i) {
+    for (std::size_t j = i; j < user_count - 1; ++j) {
       if (name_array[j] > name_array[j + 1]) {
         std::swap(name_array[j], name_array[j + 1]);
       }
     }
   }
 
-  //вывод всего моссива
-  std::cout << "Here is your sorted array" << '\n';
-  for (std::int16_t i = 0; i < user_count; i++) {
+  //вывод всего массива
+  std::cout << "Here is your sorted array\n";
+  for (std::size_t i = 0; i < user_count; ++i) {
     std::cout << i << " member of name array is " << name_array[i] << '\n';
   }
 

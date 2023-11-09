ca7fa5bcffa85d6a97b6e59c2a4ee592292daa44
atorym <atorym@gmail.com>
2023-11-09 19:26:19 +0500

    Глава 6 тест 3

--- main.cpp
+++ main.cpp
@@ -1,8 +1,30 @@
+/*
+Задание №3
+ Напишите свою функцию, которая меняет местами значения двух целочисленных переменных.
+ Проверку осуществляйте в функции main().
+ */
+
 #include <cstdlib>
+#include <iostream>
 
 
-int main(int argc, char* argv[]) {
-  <#initialize#>
+void swap(std::size_t*, std::size_t*);
 
+int main() {
+
+  std::size_t lhs{5};
+  std::size_t rhs{10};
+  std::size_t *ptr_lhs = &lhs;
+  std::size_t *ptr_rhs = &rhs;
+  std::cout << "Before swap\nlhs is " << lhs << "\nrhs is " << rhs << '\n';
+  swap(ptr_lhs, ptr_rhs);
+  std::cout << "After swap\nlhs is " << lhs << "\nrhs is " << rhs << '\n';
   return EXIT_SUCCESS;
+}
+
+void swap(std::size_t* lhs, std::size_t* rhs) {
+  std::size_t mhs;
+  mhs = *lhs;
+  *lhs = *rhs;
+  *rhs = mhs;
 }

a4995c310ed842d62d0767d332aa34f7d79070bb
atorym <atorym@gmail.com>
2024-03-27 19:55:53 +0500

    Финальный тест, задание 2.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,36 @@
-#include <cstdlib>
+/*
+Задание №2
+Напишите деструктор для следующего класса:
+*/
 
+#include <iostream>
 
-int main(int argc, char* argv[]) {
-  <#initialize#>
+class Welcome {
+private:
+  char* m_data;
 
-  return EXIT_SUCCESS;
+public:
+  Welcome() {
+    m_data = new char[14];
+    const char* init = "Hello, World!";
+    for (int i = 0; i < 14; ++i)
+      m_data[i] = init[i];
+  }
+
+  //реализация деструктора
+  ~Welcome() {
+    delete m_data;
+    m_data = nullptr;
+  }
+
+  void print() const {
+    std::cout << m_data;
+  }
+};
+
+int main() {
+  Welcome hello;
+  hello.print();
+
+  return 0;
 }

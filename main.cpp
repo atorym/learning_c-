bc834a4cb8c7784636645d78356a0346d8d8529a
atorym <atorym@gmail.com>
2024-03-27 19:55:04 +0500

    Финальный тест, задание 1.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,55 @@
-#include <cstdlib>
+/*
+Тест
+Задание №1
+a) Напишите класс с именем Point. В классе Point должны быть две переменные-члены типа double:
+m_a и m_b со значениями по умолчанию 0.0. Напишите конструктор для этого класса и функцию вывода print().
+
+b)Теперь добавим метод distanceTo(), который будет принимать второй объект класса Point в качестве
+параметра и будет вычислять расстояние между двумя объектами. Учитывая две точки (a1, b1) и (a2, b2),
+расстояние между ними можно вычислить следующим образом: sqrt((a1 - a2) * (a1 - a2) + (b1 - b2) * (b1 - b2)).
+Функция sqrt находится в заголовочном файле cmath.
+
+c) Измените функцию distanceTo() из метода класса в дружественную функцию, которая будет принимать два
+объекта класса Point в качестве параметров. Переименуйте эту функцию на distanceFrom().
+*/
+
+#include <iostream>
+#include <cmath>
 
 
-int main(int argc, char* argv[]) {
-  <#initialize#>
+class Point {
+private:
+  double m_a{0.0};
+  double m_b{0.0};
+
+public:
+  Point()= default;
+  Point(double a, double b)
+      : m_a{a}
+      , m_b{b}
+  {
+  }
+
+  void print(){
+    std::cout << "Point(" << m_a << ", " << m_b << ")\n";
+  }
+
+  friend double distanceFrom(Point, Point);
+};
+
+
+double distanceFrom(Point first, Point second){
+  return sqrt((first.m_a- second.m_a) * (first.m_a - second.m_a) + (first.m_b - second.m_b)*(first.m_b - second.m_b));
+}
+
+
+int main() {
+
+  Point first;
+  Point second(2.0, 5.0);
+  first.print();
+  second.print();
+  std::cout << "Distance between two points: " << distanceFrom(first, second) << '\n';
 
   return EXIT_SUCCESS;
 }

71e66a542c0f628ce96663da3cc00b4abe05cd8d
conelov <conelove@yandex.ru>
2024-03-29 13:02:49 +0300

    Тест урок 118_2 review

--- main.cpp
+++ main.cpp
@@ -12,28 +12,27 @@
 #include <iostream>
 #include <string_view>
 
-    struct Season
-{
-  std::string_view name{};
+
+struct Season {
+  std::string_view name;
   double averageTemperature{};
 };
 
-int main()
-{
-  std::array<Season, 4> seasons{
-    { { "Spring", 285.0 },
-      { "Summer", 296.0 },
-      { "Fall", 288.0 },
-      { "Winter", 263.0 } }
-  };
+
+int main() {
+  std::array<Season, 4> seasons{{
+    {"Spring", 285.0},
+    {"Summer", 296.0},
+    {"Fall", 288.0},
+    {"Winter", 263.0},
+  }};
 
   //Используйте std::sort() здесь
-  std::sort(seasons.begin(),seasons.end(),[](const Season lhs, const Season rhs){
-    return lhs.averageTemperature<rhs.averageTemperature;
+  std::sort(seasons.begin(), seasons.end(), [](const Season& lhs, const Season& rhs) {
+    return lhs.averageTemperature < rhs.averageTemperature;
   });
 
-  for (const auto& season : seasons)
-  {
+  for (const auto& season : seasons) {
     std::cout << season.name << '\n';
   }
   return EXIT_SUCCESS;

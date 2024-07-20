ec1f7bcc40b1004137966ff256f49cd6d798ba4f
atorym <atorym@gmail.com>
2024-07-21 04:02:58 +0500

    Тест урок 198.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
-#include <cstdlib>
+/*
+Тест
 
+Какие из следующих стейтментов, обозначенные буквами, не скомпилируются:
+   int main()
+{
+ int x;
 
-int main(int argc, char* argv[]) {
-  <#initialize#>
+ // Ссылки l-value
+ int &ref1 = x; // A                           -ok
+ int &ref2 = 7; // B                           -not ok
 
-  return EXIT_SUCCESS;
+ const int &ref3 = x; // C                     -ok
+ const int &ref4 = 7; // D                     -ok
+
+ // Ссылки r-value
+ int &&ref5 = x; // E                          -not ok
+ int &&ref6 = 7; // F                          -ok
+
+ const int &&ref7 = x; // G                    -not ok
+ const int &&ref8 = 7; // H                    -ok
+
+ return 0;
 }
+*/
